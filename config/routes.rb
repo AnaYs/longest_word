f5aaa86972e5549ff1c33af691f477a9739c1458
Anna Isabel Lamers <isabel.lamers@gmail.com>
2015-06-08 16:50:16 +0200

    longest word app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'game', to: 'grid#game'
+
+  get 'score', to: 'grid#score'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
